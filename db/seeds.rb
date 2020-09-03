356aaf763af75075c4365dd83e9701b4ea630a14
Marisa Tsubokura <marieee090530@gmail.com>
2020-09-03 22:50:19 +1000

    create front end parts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Createing..."
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody mary")
+Cocktail.create(name: "Sex on the beach")
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Pina Colada")
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "orange juce")
+Ingredient.create(name: "tomato juce")
+
+puts "Finish!"
